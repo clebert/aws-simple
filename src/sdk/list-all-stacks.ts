7f0208461cbe62b0fcce070127b13d60962bbfa8
Clemens Akens <clebert@me.com>
2019-10-08 11:12:07 +0200

    Sort and format output of the `list` command

--- src/sdk/list-all-stacks.ts
+++ src/sdk/list-all-stacks.ts
@@ -1,7 +1,22 @@
 import {CloudFormation} from 'aws-sdk';
+import chalk from 'chalk';
+import createUi from 'cliui';
 import {DeploymentDescriptor} from '../utils/deployment-descriptor';
 import {SdkConfig, createClientConfig} from './create-client-config';
 import {findAllStacks} from './find-all-stacks';
+
+function getLastUpdatedTime(stack: CloudFormation.Stack): Date {
+  return stack.LastUpdatedTime || stack.CreationTime;
+}
+
+function compareLastUpdatedTimes(
+  stack1: CloudFormation.Stack,
+  stack2: CloudFormation.Stack
+): number {
+  return (
+    getLastUpdatedTime(stack2).getTime() - getLastUpdatedTime(stack1).getTime()
+  );
+}
 
 export async function listAllStacks(
   deploymentDescriptor: DeploymentDescriptor,
@@ -11,7 +26,29 @@ export async function listAllStacks(
   const cloudFormation = new CloudFormation(clientConfig);
   const stacks = await findAllStacks(deploymentDescriptor, cloudFormation);
 
-  for (const stack of stacks) {
-    console.log(stack.StackName);
+  for (const stack of stacks.sort(compareLastUpdatedTimes)) {
+    if (stack.DeletionTime) {
+      continue;
+    }
+
+    const ui = createUi({wrap: true});
+
+    ui.div(
+      chalk.bold('Stack ID'),
+      chalk.bold('Last Updated'),
+      chalk.bold('Tags')
+    );
+
+    const {StackName, Tags} = stack;
+
+    ui.div(
+      StackName,
+      getLastUpdatedTime(stack).toString(),
+      Tags && Tags.length > 0
+        ? Tags.map(({Key, Value}) => `${Key}=${Value}`).join(', ')
+        : ''
+    );
+
+    console.info(ui.toString());
   }
 }
