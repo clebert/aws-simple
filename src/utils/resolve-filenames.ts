e830426991f5c912ffa2b6263c1306888fd482ed
Clemens Akens <clebert@me.com>
2019-10-10 21:53:02 +0200

    Fix the handling of nested directories

--- src/utils/resolve-filenames.ts
+++ src/utils/resolve-filenames.ts
@@ -1,14 +1,27 @@
-import {readdirSync} from 'fs';
+import {existsSync, lstatSync, readdirSync} from 'fs';
 import * as path from 'path';
 import {S3Config} from '..';
 
 export function resolveFilenames(s3Config: S3Config): string[] {
   const {type, localPath} = s3Config;
 
-  const filenames =
-    type === 'file'
-      ? [localPath]
-      : readdirSync(localPath).map(filename => path.join(localPath, filename));
+  if (type === 'file') {
+    if (!existsSync(localPath) || !lstatSync(localPath).isFile()) {
+      throw new Error(
+        `Expect the specified local path to be an existing file: ${localPath}`
+      );
+    }
 
-  return filenames.map(filename => path.resolve(filename));
+    return [path.resolve(localPath)];
+  }
+
+  if (!existsSync(localPath) || !lstatSync(localPath).isDirectory()) {
+    throw new Error(
+      `Expect the specified local path to be an existing directory: ${localPath}`
+    );
+  }
+
+  return readdirSync(localPath)
+    .map(filename => path.resolve(path.join(localPath, filename)))
+    .filter(filename => lstatSync(filename).isFile());
 }
