9cc79e56076c9faf9061759f80bdeecee37aa7dd
Clemens Akens <clebert@me.com>
2019-10-23 12:19:37 +0200

    Fix an error in generation of logical IDs

--- src/utils/create-short-hash.ts
+++ src/utils/create-short-hash.ts
@@ -1,9 +1,11 @@
 import {createHash} from 'crypto';
 
 export function createShortHash(...values: string[]): string {
-  return createHash('sha1')
+  const shortHash = createHash('sha1')
     .update(JSON.stringify(values))
     .digest()
     .toString('hex')
     .slice(0, 7);
+
+  return `R${shortHash}`;
 }
