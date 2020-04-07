c3aab2c4199314426db897c622ea75bb57b97586
Clemens Akens <clebert@me.com>
2020-04-07 14:29:47 +0200

    Add unit test that asserts the behavior of a private Express API

--- src/express/utils/remove-all-routes.ts
+++ src/express/utils/remove-all-routes.ts
@@ -1,7 +1,13 @@
-import express from 'express';
+export interface ExpressAppPrivateApi {
+  _router?: {
+    stack: {
+      route?: object;
+    }[];
+  };
+}
 
-export function removeAllRoutes(app: express.Express): void {
-  app._router.stack = app._router.stack.filter(
-    (stack: {readonly route?: object}) => !stack.route
-  );
+export function removeAllRoutes(app: ExpressAppPrivateApi): void {
+  if (app._router) {
+    app._router.stack = app._router.stack.filter((stack) => !stack.route);
+  }
 }
