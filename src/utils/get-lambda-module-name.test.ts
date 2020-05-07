1be3e6c03163c94b0e3a4bcdf1cc34490e3b2c71
Moritz Behnke <moritz.behnke@sinnerschrader.com>
2020-05-07 17:03:54 +0200

    Adapt regex for lambda module name to accept characters allowed by AWS

--- src/utils/get-lambda-module-name.test.ts
+++ src/utils/get-lambda-module-name.test.ts
@@ -5,16 +5,18 @@ describe('getLambdaModuleName()', () => {
     expect(getLambdaModuleName('index.js')).toBe('index');
     expect(getLambdaModuleName('foo/index.js')).toBe('index');
     expect(getLambdaModuleName('ABCabc.js')).toBe('ABCabc');
+    expect(getLambdaModuleName('ABC012.js')).toBe('ABC012');
+    expect(getLambdaModuleName('abc-abc.js')).toBe('abc-abc');
+    expect(getLambdaModuleName('abc_abc.js')).toBe('abc_abc');
   });
 
   it('checks the file name for valid characters', () => {
     const expectedError = new Error(
-      'The Lambda file name (without file extension) must match the following pattern: /^[A-Za-z]+$/'
+      'The Lambda file name (without file extension) must match the following pattern: /^[A-Za-z0-9-_]+$/'
     );
 
     expect(() => getLambdaModuleName('.js')).toThrow(expectedError);
     expect(() => getLambdaModuleName('foo/.js')).toThrow(expectedError);
     expect(() => getLambdaModuleName('index.bundle.js')).toThrow(expectedError);
-    expect(() => getLambdaModuleName('index-bundle.js')).toThrow(expectedError);
   });
 });
