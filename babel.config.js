8912962a20f33722db5c027f773841c5cb938241
Clemens Akens <clebert@me.com>
2020-12-02 15:58:55 +0100

    Improve project configs

--- babel.config.js
+++ babel.config.js
@@ -4,9 +4,4 @@ module.exports = {
     '@babel/typescript',
     '@babel/react',
   ],
-  plugins: [
-    '@babel/proposal-class-properties',
-    '@babel/proposal-nullish-coalescing-operator',
-    '@babel/proposal-optional-chaining',
-  ],
 };
