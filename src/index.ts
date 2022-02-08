e1433e5cba91669f0909692fbad3cf2608128943
Clemens Akens <clebert@me.com>
2022-02-08 15:33:35 +0100

    Refactor index file

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,45 @@ import {upload} from './upload';
 
 export type GetStackConfig = typeof getStackConfig;
 
+const buildSynthesizeCommand: yargs.BuilderCallback<{}, {}> = (argv) =>
+  argv.example(`npx cdk deploy --app 'npx $0 synthesize'`, ``);
+
+const buildUploadCommand: yargs.BuilderCallback<{}, {}> = (argv) =>
+  argv
+    .describe(`yes`, `Automatically confirm the upload`)
+    .boolean(`yes`)
+    .default(`yes`, false)
+
+    .example(`npx $0 upload`, ``)
+    .example(`npx $0 upload --yes`, ``);
+
+const buildListCommand: yargs.BuilderCallback<{}, {}> = (argv) =>
+  argv
+    .describe(
+      `all`,
+      `List all deployed stacks no matter which domain name they belong to`,
+    )
+    .boolean(`all`)
+    .default(`all`, false)
+
+    .describe(
+      `hosted-zone-name`,
+      `List the deployed stacks that belong to the specified hosted zone name, ` +
+        `if none is specified, the hosted zone name is read from the config file`,
+    )
+    .string(`hosted-zone-name`)
+
+    .describe(
+      `legacy-app-name`,
+      `List the deployed stacks that belong to the specified app name`,
+    )
+    .string(`legacy-app-name`)
+
+    .example(`npx $0 list`, ``)
+    .example(`npx $0 list --all`, ``)
+    .example(`npx $0 list --domain-name=example.com`, ``)
+    .example(`npx $0 list --legacy-app-name=example`, ``);
+
 (async () => {
   const {description} = require(`../package.json`);
 
@@ -20,51 +59,13 @@ export type GetStackConfig = typeof getStackConfig;
     .demandCommand()
     .epilogue(description)
     .strict()
-
     .command(
       `synthesize [options]`,
       `Synthesize a stack using the CDK`,
-      (commandArgv) =>
-        commandArgv.example(`npx cdk deploy --app 'npx $0 synthesize'`, ``),
+      buildSynthesizeCommand,
     )
-
-    .command(`upload [options]`, `Upload files to S3`, (commandArgv) =>
-      commandArgv
-        .describe(`yes`, `Automatically confirm the upload`)
-        .boolean(`yes`)
-        .default(`yes`, false)
-
-        .example(`npx $0 upload`, ``)
-        .example(`npx $0 upload --yes`, ``),
-    )
-
-    .command(`list [options]`, `List deployed stacks`, (commandArgv) =>
-      commandArgv
-        .describe(
-          `all`,
-          `List all deployed stacks no matter which domain name they belong to`,
-        )
-        .boolean(`all`)
-        .default(`all`, false)
-
-        .describe(
-          `hosted-zone-name`,
-          `List the deployed stacks that belong to the specified hosted zone name, ` +
-            `if none is specified, the hosted zone name is read from the config file`,
-        )
-        .string(`hosted-zone-name`)
-
-        .describe(
-          `legacy-app-name`,
-          `List the deployed stacks that belong to the specified app name`,
-        )
-        .string(`legacy-app-name`)
-
-        .example(`npx $0 list`, ``)
-        .example(`npx $0 list --all`, ``)
-        .example(`npx $0 list --domain-name=example.com`, ``)
-        .example(`npx $0 list --legacy-app-name=example`, ``),
-    );
+    .command(`upload [options]`, `Upload files to S3`, buildUploadCommand)
+    .command(`list [options]`, `List deployed stacks`, buildListCommand);
 
   const argv = cli.argv as any;
 
