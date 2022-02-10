d24c27ce6302e0fee88a0c4d955d9e0d3480ff6b
Clemens Akens <clebert@me.com>
2022-02-10 17:20:36 +0100

    Delete the bucket from legacy stacks manually

--- src/sdk/delete-stack.ts
+++ src/sdk/delete-stack.ts
@@ -3,9 +3,21 @@ import {
   DeleteStackCommand,
   waitUntilStackDeleteComplete,
 } from '@aws-sdk/client-cloudformation';
+import {deleteBucket} from './delete-bucket';
+import {findStack} from './find-stack';
+import {getOutputValue} from './get-output-value';
 
 export async function deleteStack(stackName: string): Promise<void> {
   const client = new CloudFormationClient({});
+
+  if (stackName.startsWith(`aws-simple--`)) {
+    const stack = await findStack(stackName);
+    const bucketName = getOutputValue(stack, `BucketName`);
+
+    if (bucketName) {
+      await deleteBucket(bucketName);
+    }
+  }
 
   await client.send(new DeleteStackCommand({StackName: stackName}));
 
