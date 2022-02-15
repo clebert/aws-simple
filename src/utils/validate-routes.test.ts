cab17ae50c6b455968e74c42741adca10aa86933
Clemens Akens <clebert@me.com>
2022-02-15 14:27:31 +0100

    Improve stack config validation

--- src/utils/validate-routes.test.ts
+++ src/utils/validate-routes.test.ts
@@ -1,73 +1,123 @@
+import type {LambdaRoute, S3Route} from '../read-stack-config';
 import {validateRoutes} from './validate-routes';
+
+const lambdaRoute: Omit<LambdaRoute, 'publicPath' | 'functionName'> = {
+  type: `function`,
+  httpMethod: `GET`,
+  path: ``,
+};
+
+const s3FileRoute: Omit<S3Route, 'publicPath'> = {type: `file`, path: ``};
+const s3FolderRoute: Omit<S3Route, 'publicPath'> = {type: `folder`, path: ``};
 
 describe(`validateRoutes()`, () => {
   test(`does not throw an error`, () => {
     validateRoutes([]);
 
     validateRoutes([
-      {type: `file`, publicPath: `/`, path: ``},
-      {type: `folder`, publicPath: `/*`, path: ``},
-      {
-        type: `function`,
-        httpMethod: `GET`,
-        publicPath: `/foo`,
-        path: ``,
-        functionName: `foobar`,
-      },
-      {
-        type: `function`,
-        httpMethod: `GET`,
-        publicPath: `/foo/*`,
-        path: ``,
-        functionName: `foo_bar`,
-      },
+      {...s3FileRoute, publicPath: `/foo`},
+      {...s3FileRoute, publicPath: `/*`},
+    ]);
+
+    validateRoutes([
+      {...s3FileRoute, publicPath: `/`},
+      {...s3FolderRoute, publicPath: `/*`},
+    ]);
+
+    validateRoutes([
+      {...s3FolderRoute, publicPath: `/*`},
+      {...s3FileRoute, publicPath: `/`},
+    ]);
+
+    validateRoutes([
+      {...lambdaRoute, publicPath: `/`, functionName: `foo`},
+      {...s3FolderRoute, publicPath: `/*`},
+    ]);
+
+    validateRoutes([
+      {...s3FolderRoute, publicPath: `/*`},
+      {...lambdaRoute, publicPath: `/`, functionName: `foo`},
     ]);
   });
 
   test(`throws an error`, () => {
     expect(() =>
       validateRoutes([
-        {type: `file`, publicPath: `/*`, path: ``},
-        {type: `folder`, publicPath: `/*`, path: ``},
+        {...s3FileRoute, publicPath: `/`},
+        {...s3FileRoute, publicPath: `/*`},
       ]),
-    ).toThrow(new Error(`A public path must be unique: /*`));
+    ).toThrow(new Error(`A public path must be unique: /`));
 
     expect(() =>
-      validateRoutes([{type: `file`, publicPath: `/*/`, path: ``}]),
-    ).toThrow(
-      new Error(`A public path other than root must not end with a slash: /*/`),
-    );
+      validateRoutes([
+        {...s3FileRoute, publicPath: `/*`},
+        {...s3FileRoute, publicPath: `/`},
+      ]),
+    ).toThrow(new Error(`A public path must be unique: /`));
 
     expect(() =>
-      validateRoutes([{type: `file`, publicPath: `/*/foo`, path: ``}]),
-    ).toThrow(
-      new Error(`A public path may contain a wildcard only at the end: /*/foo`),
-    );
+      validateRoutes([
+        {...lambdaRoute, publicPath: `/`, functionName: `foo`},
+        {...lambdaRoute, publicPath: `/*`, functionName: `bar`},
+      ]),
+    ).toThrow(new Error(`A public path must be unique: /`));
 
     expect(() =>
-      validateRoutes([{type: `folder`, publicPath: `/`, path: ``}]),
+      validateRoutes([
+        {...lambdaRoute, publicPath: `/*`, functionName: `foo`},
+        {...lambdaRoute, publicPath: `/`, functionName: `bar`},
+      ]),
+    ).toThrow(new Error(`A public path must be unique: /`));
+
+    expect(() =>
+      validateRoutes([
+        {...s3FileRoute, publicPath: `/`},
+        {...lambdaRoute, publicPath: `/*`, functionName: `foo`},
+      ]),
+    ).toThrow(new Error(`A public path must be unique: /`));
+
+    expect(() =>
+      validateRoutes([
+        {...lambdaRoute, publicPath: `/`, functionName: `foo`},
+        {...s3FileRoute, publicPath: `/*`},
+      ]),
+    ).toThrow(new Error(`A public path must be unique: /`));
+
+    expect(() =>
+      validateRoutes([
+        {...s3FileRoute, publicPath: `/foo`},
+        {...s3FileRoute, publicPath: `/foo/*`},
+      ]),
+    ).toThrow(new Error(`A public path must be unique: /foo`));
+
+    expect(() =>
+      validateRoutes([{...s3FileRoute, publicPath: `/foo/`}]),
     ).toThrow(
       new Error(
-        `A public path of an S3 folder route must end with a wildcard: /`,
+        `A public path other than root must not end with a slash: /foo/`,
+      ),
+    );
+
+    expect(() =>
+      validateRoutes([{...s3FileRoute, publicPath: `/foo/*/bar`}]),
+    ).toThrow(
+      new Error(
+        `A public path may contain a wildcard only at the end: /foo/*/bar`,
+      ),
+    );
+
+    expect(() =>
+      validateRoutes([{...s3FolderRoute, publicPath: `/foo`}]),
+    ).toThrow(
+      new Error(
+        `A public path of an S3 folder route must end with a wildcard: /foo`,
       ),
     );
 
     expect(() =>
       validateRoutes([
-        {
-          type: `function`,
-          httpMethod: `GET`,
-          publicPath: `/foo`,
-          path: ``,
-          functionName: `foo bar`,
-        },
-        {
-          type: `function`,
-          httpMethod: `GET`,
-          publicPath: `/foo/*`,
-          path: ``,
-          functionName: `foo_bar`,
-        },
+        {...lambdaRoute, publicPath: `/foo`, functionName: `foo bar`},
+        {...lambdaRoute, publicPath: `/bar`, functionName: `foo_bar`},
       ]),
     ).toThrow(new Error(`A normalized function name must be unique: foo-bar`));
   });
--- src/utils/validate-routes.ts
+++ src/utils/validate-routes.ts
@@ -2,44 +2,53 @@ import type {Route} from '../read-stack-config';
 import {getNormalizedName} from './get-normalized-name';
 
 export function validateRoutes(routes: readonly Route[]): void {
-  const publicPaths = new Set<string>();
-  const functionNames = new Set<string>();
+  const existingPublicPaths = new Set<string>();
+  const existingFunctionNames = new Set<string>();
 
   for (const route of routes) {
-    const {type, publicPath} = route;
+    const publicPaths =
+      route.type === `folder`
+        ? [route.publicPath]
+        : route.publicPath.endsWith(`/*`)
+        ? [route.publicPath, route.publicPath.replace(`/*`, ``) || `/`]
+        : [route.publicPath];
 
-    if (publicPaths.has(publicPath)) {
-      throw new Error(`A public path must be unique: ${publicPath}`);
+    for (const publicPath of publicPaths) {
+      if (existingPublicPaths.has(publicPath)) {
+        throw new Error(`A public path must be unique: ${publicPath}`);
+      }
+
+      existingPublicPaths.add(publicPath);
+
+      if (publicPath !== `/` && publicPath.endsWith(`/`)) {
+        throw new Error(
+          `A public path other than root must not end with a slash: ${publicPath}`,
+        );
+      }
+
+      if (/\/\*.+/.test(publicPath)) {
+        throw new Error(
+          `A public path may contain a wildcard only at the end: ${publicPath}`,
+        );
+      }
+
+      if (route.type === `folder` && !publicPath.endsWith(`/*`)) {
+        throw new Error(
+          `A public path of an S3 folder route must end with a wildcard: ${publicPath}`,
+        );
+      }
     }
 
-    if (publicPath !== `/` && publicPath.endsWith(`/`)) {
-      throw new Error(
-        `A public path other than root must not end with a slash: ${publicPath}`,
-      );
-    }
-
-    publicPaths.add(publicPath);
-
-    if (/\/\*.+/.test(publicPath)) {
-      throw new Error(
-        `A public path may contain a wildcard only at the end: ${publicPath}`,
-      );
-    }
-
-    if (type === `function`) {
+    if (route.type === `function`) {
       const functionName = getNormalizedName(route.functionName);
 
-      if (functionNames.has(functionName)) {
+      if (existingFunctionNames.has(functionName)) {
         throw new Error(
           `A normalized function name must be unique: ${functionName}`,
         );
       }
 
-      functionNames.add(functionName);
-    } else if (type === `folder` && !publicPath.endsWith(`/*`)) {
-      throw new Error(
-        `A public path of an S3 folder route must end with a wildcard: ${publicPath}`,
-      );
+      existingFunctionNames.add(functionName);
     }
   }
 }
