09d13440ababc386ca6101c57f015cd9ecbe7bd1
Hendrik Liebau <mail@hendrik-liebau.de>
2022-02-17 11:26:40 +0100

    Print termination protection status in list command

--- src/sdk/find-stack.ts
+++ src/sdk/find-stack.ts
@@ -1,10 +1,17 @@
 import type {Stack} from '@aws-sdk/client-cloudformation';
-import {findStacks} from './find-stacks';
+import {
+  CloudFormationClient,
+  DescribeStacksCommand,
+} from '@aws-sdk/client-cloudformation';
 
 export async function findStack(stackName: string): Promise<Stack> {
-  const stack = (await findStacks()).find(
-    ({StackName}) => StackName === stackName,
+  const client = new CloudFormationClient({});
+
+  const {Stacks} = await client.send(
+    new DescribeStacksCommand({StackName: stackName}),
   );
+
+  const stack = Stacks?.[0];
 
   if (!stack) {
     throw new Error(`The stack cannot be found: ${stackName}`);
