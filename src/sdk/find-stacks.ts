bcee7931864f7dba1617fd03274dc2214b54ff39
Hendrik Liebau <mail@hendrik-liebau.de>
2023-08-09 09:28:44 +0200

    Use `aws-sdk` paginators for pagination

--- src/sdk/find-stacks.ts
+++ src/sdk/find-stacks.ts
@@ -4,43 +4,41 @@ import {findStack} from './find-stack.js';
 import {getOutputValue} from './get-output-value.js';
 import {
   CloudFormationClient,
-  DescribeStacksCommand,
+  paginateDescribeStacks,
 } from '@aws-sdk/client-cloudformation';
+
+type NamedStack = Stack & {StackName: string};
 
 export async function findStacks(
   hostedZoneName?: string,
 ): Promise<readonly Stack[]> {
   const client = new CloudFormationClient({});
-  const stacks: Stack[] = [];
+  const stacks: NamedStack[] = [];
+  const paginator = paginateDescribeStacks({client}, {});
 
-  let nextToken: string | undefined;
-
-  do {
-    const output = await client.send(
-      new DescribeStacksCommand({NextToken: nextToken}),
-    );
-
+  for await (const output of paginator) {
     if (output.Stacks) {
-      stacks.push(...output.Stacks);
+      for (const stack of output.Stacks) {
+        if (isMatchingStack(stack, hostedZoneName)) {
+          stacks.push(stack);
+        }
+      }
     }
-
-    nextToken = output.NextToken;
-  } while (nextToken);
+  }
 
   return Promise.all(
     stacks
-      .filter((stack) => isMatchingStack(stack, hostedZoneName))
       // For some reason `stack.EnableTerminationProtection` is always
       // `undefined`. Describing a single stack instead, does return the correct
       // value (true or false) for `EnableTerminationProtection`.
-      .map(async ({StackName}) => findStack(StackName!)),
+      .map(async ({StackName}) => findStack(StackName)),
   );
 }
 
 function isMatchingStack(
   stack: Stack,
   hostedZoneName: string | undefined,
-): boolean {
+): stack is NamedStack {
   if (!stack.StackName?.startsWith(`aws-simple`)) {
     return false;
   }
