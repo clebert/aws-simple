2ccb722840b6c5b5644045e13a026315cd0b9054
Clemens Akens <clebert@me.com>
2025-07-25 13:27:27 +0200

    Update configs and dependencies

--- ci.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e           # Terminates script at the first error
-set -o pipefail  # Sets the exit status for pipes
-set -u           # Triggers an error when an unset variable is called
-set -o noclobber # Prevents from overwriting existing files
-npm run ci
